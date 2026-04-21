5e1793f853a811914ad366a02462160647366ecf
kwx1060333 <korovkin.dmitry1@huawei-partners.com>
2026-04-22 07:17:20 +0800

    Add tests for TDeQuant
    Co-authored-by: kwx1060333<korovkin.dmitry1@huawei-partners.com>
    
    # message auto-generated for no-merge-commit merge:
    !808 merge add-tests-dequant into master
    
    Add tests for TDeQuant
    
    Created-by: kwx1060333
    Commit-by: kwx1060333
    Merged-by: cann-robot
    Description: ## 描述
    Add tests for TDeQuant
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    See merge request: cann/pto-isa!808
    
    (cherry picked from commit 46c942ee74abc2bc64dacd7a49c9c10e9f2cad7c)

--- tests/cpu/st/testcase/tdequant/main.cpp
+++ tests/cpu/st/testcase/tdequant/main.cpp
@@ -8,54 +8,12 @@ INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A
 See LICENSE in the root of the software repository for the full text of the License.
 */
 
-#include "cpu_tile_test_utils.h"
 #include "test_common.h"
 #include <gtest/gtest.h>
 #include <pto/pto-inst.hpp>
 
 using namespace std;
-using namespace pto;
-using namespace CpuTileTestUtils;
 using namespace PtoTestCommon;
-
-namespace {
-
-TEST(TDequantTest, AppliesPerRowBroadcastedScaleAndOffset)
-{
-    using DstTile = Tile<TileType::Vec, float, 2, 8, BLayout::RowMajor, 2, 4>;
-    using SrcTile = Tile<TileType::Vec, int32_t, 2, 8, BLayout::RowMajor, 2, 4>;
-    using ParaTile = Tile<TileType::Vec, float, 2, 8, BLayout::RowMajor, 2, 1>;
-
-    DstTile dst;
-    SrcTile src;
-    ParaTile scale;
-    ParaTile offset;
-    std::size_t addr = 0;
-    AssignTileStorage(addr, dst, src, scale, offset);
-
-    const int srcValues[2][4] = {{3, 5, 7, 9}, {10, 12, 14, 16}};
-    const float scaleValues[2] = {0.5f, 2.0f};
-    const float offsetValues[2] = {1.0f, 1.5f};
-
-    for (int r = 0; r < src.GetValidRow(); ++r) {
-        SetValue(scale, r, 0, scaleValues[r]);
-        SetValue(offset, r, 0, offsetValues[r]);
-        for (int c = 0; c < src.GetValidCol(); ++c) {
-            SetValue(src, r, c, srcValues[r][c]);
-        }
-    }
-
-    TDEQUANT(dst, src, scale, offset);
-
-    for (int r = 0; r < dst.GetValidRow(); ++r) {
-        for (int c = 0; c < dst.GetValidCol(); ++c) {
-            const float expected = (static_cast<float>(srcValues[r][c]) - offsetValues[r]) * scaleValues[r];
-            ExpectValueEquals(GetValue(dst, r, c), expected);
-        }
-    }
-}
-
-} // namespace
 
 template <typename dstType, typename srcType, int kTRows_, int kTCols_, int vRows, int vCols>
 void launchTDequant(dstType *out, srcType *src, dstType *scale, dstType *offset, void *stream);
