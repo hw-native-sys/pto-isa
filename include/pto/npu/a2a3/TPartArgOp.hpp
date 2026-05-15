049c9c798030ec70af82d6662cad2bdae9aa0773
denglei0610 <denglei31@h-partners.com>
2026-05-15 14:24:12 +0800

    Fix TPARTARGMIN TPARTARGMAX issue when tile size differs
    Co-authored-by: denglei0610<denglei31@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !924 merge fix into master
    
    Fix TPARTARGMIN TPARTARGMAX issue when tile size differs
    
    Created-by: denglei0610
    Commit-by: denglei0610
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    修复RowStride指定错误导致的用例失败问题。
    修复用例代码中写错的地方。
    修复用例中tilesize总合过大的用例。
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    https://gitcode.com/cann/pto-isa/issues/342
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/pto-isa!924

--- include/pto/npu/a2a3/TPartArgOp.hpp
+++ include/pto/npu/a2a3/TPartArgOp.hpp
@@ -61,7 +61,7 @@ PTO_INTERNAL void TPartArgOps(__ubuf__ TVal *dstValPtr, __ubuf__ TIdx *dstIdxPtr
             pipe_barrier(PIPE_V);
             vsel(dstValPtr + row * TileDstVal::RowStride + i * elementsPerRepeat,
                  srcVal0Ptr + row * TileSrcVal0::RowStride + i * elementsPerRepeat,
-                 srcVal1Ptr + row * TileSrcVal0::RowStride + i * elementsPerRepeat, 1, 1, 1, 1, 0, 0, 0);
+                 srcVal1Ptr + row * TileSrcVal1::RowStride + i * elementsPerRepeat, 1, 1, 1, 1, 0, 0, 0);
             vsel((__ubuf__ TVal *)dstIdxPtr + row * TileDstIdx::RowStride + i * elementsPerRepeat,
                  (__ubuf__ TVal *)srcIdx0Ptr + row * TileSrcIdx0::RowStride + i * elementsPerRepeat,
                  (__ubuf__ TVal *)srcIdx1Ptr + row * TileSrcIdx1::RowStride + i * elementsPerRepeat, 1, 1, 1, 1, 0, 0,
@@ -78,7 +78,7 @@ PTO_INTERNAL void TPartArgOps(__ubuf__ TVal *dstValPtr, __ubuf__ TIdx *dstIdxPtr
             pipe_barrier(PIPE_V);
             vsel(dstValPtr + row * TileDstVal::RowStride + completeRepeats * elementsPerRepeat,
                  srcVal0Ptr + row * TileSrcVal0::RowStride + completeRepeats * elementsPerRepeat,
-                 srcVal1Ptr + row * TileSrcVal0::RowStride + completeRepeats * elementsPerRepeat, 1, 1, 1, 1, 0, 0, 0);
+                 srcVal1Ptr + row * TileSrcVal1::RowStride + completeRepeats * elementsPerRepeat, 1, 1, 1, 1, 0, 0, 0);
             vsel((__ubuf__ TVal *)dstIdxPtr + row * TileDstIdx::RowStride + completeRepeats * elementsPerRepeat,
                  (__ubuf__ TVal *)srcIdx0Ptr + row * TileSrcIdx0::RowStride + completeRepeats * elementsPerRepeat,
                  (__ubuf__ TVal *)srcIdx1Ptr + row * TileSrcIdx1::RowStride + completeRepeats * elementsPerRepeat, 1, 1,
