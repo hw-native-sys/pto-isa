0dbf56dfd2c12790269f290d5c32a9a4f6e7675f
nmkihail <nesterov.mikhail3@h-partners.com>
2026-06-17 20:59:03 +0800

    Added supporting Tconcat with dstIdx, srcIdx tiles
    Co-authored-by: nmkihail<nesterov.mikhail3@h-partners.com>
    
    !1118 merge tconcat-add-idx into master
    
    Added supporting Tconcat with dstIdx, srcIdx tiles
    
    Created-by: nmkihail
    Commit-by: nmkihail
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    See merge request: cann/pto-isa!1118

--- include/pto/cpu/TConcat.hpp
+++ include/pto/cpu/TConcat.hpp
@@ -14,24 +14,131 @@ See LICENSE in the root of the software repository for the full text of the Lice
 #include "pto/cpu/tile_offsets.hpp"
 
 namespace pto {
-template <typename TileDataDst, typename TileDataSrc0, typename TileDataSrc1>
-PTO_INTERNAL void TCONCAT_IMPL(TileDataDst &dst, TileDataSrc0 &src0, TileDataSrc1 &src1)
+template <typename TileDst, typename TileSrc0, typename TileSrc1>
+PTO_INTERNAL void TConcatTileCheck(TileDst &dst, TileSrc0 &src0, TileSrc1 &src1)
 {
-    assert(dst.GetValidRow() == src0.GetValidRow() && dst.GetValidRow() == src1.GetValidRow());
-    assert(dst.GetValidCol() >= src0.GetValidCol() + src1.GetValidCol());
-    const unsigned rows = dst.GetValidRow();
-    const unsigned cols0 = src0.GetValidCol();
-    const unsigned cols1 = src1.GetValidCol();
+    using T = typename TileDst::DType;
 
+    static_assert(std::is_same_v<T, typename TileSrc0::DType> && std::is_same_v<T, typename TileSrc1::DType>,
+                  "TCONCAT: Data type of dst, src0 and src1 must be the same.");
+    static_assert(std::is_same_v<T, int32_t> || std::is_same_v<T, int16_t> || std::is_same_v<T, int8_t> ||
+                      std::is_same_v<T, uint32_t> || std::is_same_v<T, uint16_t> || std::is_same_v<T, uint8_t> ||
+                      std::is_same_v<T, half> || std::is_same_v<T, float> || std::is_same_v<T, bfloat16_t>,
+                  "TCONCAT: Invalid data type.");
+    static_assert(TileDst::Loc == TileType::Vec && TileSrc0::Loc == TileType::Vec && TileSrc1::Loc == TileType::Vec,
+                  "TCONCAT: TileType of src and dst tiles must be TileType::Vec.");
+
+    assert(dst.GetValidRow() == src0.GetValidRow() && dst.GetValidRow() == src1.GetValidRow() &&
+           "TCONCAT: Valid rows of dst, src0 and src1 tiles should be the same");
+
+    assert(dst.GetValidCol() == src0.GetValidCol() + src1.GetValidCol() &&
+           "TCONCAT: Valid cols of src1 and src1Idx tiles should be the same");
+}
+
+template <typename TileDst, typename TileSrc0, typename TileSrc1, typename TileSrc0Idx, typename TileSrc1Idx>
+PTO_INTERNAL void TConcatIdxTileCheck(TileDst &dst, TileSrc0 &src0, TileSrc1 &src1, TileSrc0Idx &src0Idx,
+                                      TileSrc1Idx &src1Idx)
+{
+    TConcatTileCheck<TileDst, TileSrc0, TileSrc1>(dst, src0, src1);
+    using TIdx = typename TileSrc0Idx::DType;
+
+    static_assert(std::is_same_v<TIdx, typename TileSrc1Idx::DType>,
+                  "TCONCAT: Data type of src0Idx and src1Idx must be the same.");
+    static_assert(std::is_same_v<TIdx, int32_t> || std::is_same_v<TIdx, int16_t> || std::is_same_v<TIdx, int8_t> ||
+                      std::is_same_v<TIdx, uint32_t> || std::is_same_v<TIdx, uint16_t> || std::is_same_v<TIdx, uint8_t>,
+                  "TCONCAT: Invalid data type of src0Idx.");
+    static_assert(TileSrc0Idx::Loc == TileType::Vec && TileSrc1Idx::Loc == TileType::Vec,
+                  "TCONCAT: TileType of src0Idx and src1Idx tiles must be TileType::Vec.");
+}
+
+template <typename TileDst, typename TileSrc0, typename TileSrc1, typename TileDstIdx, typename TileSrc0Idx,
+          typename TileSrc1Idx>
+PTO_INTERNAL void TConcatIdxTileCheck(TileDst &dst, TileSrc0 &src0, TileSrc1 &src1, TileDstIdx &dstIdx,
+                                      TileSrc0Idx &src0Idx, TileSrc1Idx &src1Idx)
+{
+    TConcatIdxTileCheck<TileDst, TileSrc0, TileSrc1, TileSrc0Idx, TileSrc1Idx>(dst, src0, src1, src0Idx, src1Idx);
+
+    static_assert(std::is_same_v<typename TileDstIdx::DType, typename TileSrc0Idx::DType>,
+                  "TCONCAT: Data type of dstIdx and src0Idx must be the same.");
+    static_assert(TileDstIdx::Loc == TileType::Vec, "TCONCAT: TileType of dstIdx tile must be TileType::Vec.");
+}
+
+template <typename TileDst, typename TileSrc0, typename TileSrc1, typename ConcatCaller>
+PTO_INTERNAL void TConcatProcessor(TileDst &dst, TileSrc0 &src0, TileSrc1 &src1, ConcatCaller &&caller)
+{
+    const unsigned rows = dst.GetValidRow();
     for (unsigned r = 0; r < rows; ++r) {
+        const auto [cols0, cols1] = caller.GetColsNumber(r);
         for (unsigned c = 0; c < cols0; ++c) {
-            dst.data()[GetTileElementOffset<TileDataDst>(r, c)] = src0.data()[GetTileElementOffset<TileDataSrc0>(r, c)];
+            dst.data()[GetTileElementOffset<TileDst>(r, c)] = src0.data()[GetTileElementOffset<TileSrc0>(r, c)];
         }
         for (unsigned c = 0; c < cols1; ++c) {
-            dst.data()[GetTileElementOffset<TileDataDst>(r, cols0 + c)] =
-                src1.data()[GetTileElementOffset<TileDataSrc1>(r, c)];
+            dst.data()[GetTileElementOffset<TileDst>(r, cols0 + c)] = src1.data()[GetTileElementOffset<TileSrc1>(r, c)];
         }
     }
+}
+
+struct TConCatCols {
+    unsigned cols0;
+    unsigned cols1;
+
+    PTO_INTERNAL std::pair<unsigned, unsigned> GetColsNumber(unsigned r)
+    {
+        return {cols0, cols1};
+    }
+};
+
+template <typename TileSrc0Idx, typename TileSrc1Idx>
+struct TConCatIdxCols {
+    TileSrc0Idx &src0Idx;
+    TileSrc1Idx &src1Idx;
+
+    PTO_INTERNAL std::pair<unsigned, unsigned> GetColsNumber(unsigned r)
+    {
+        unsigned cols0 = src0Idx.data()[GetTileElementOffset<TileSrc0Idx>(r, 0)];
+        unsigned cols1 = src1Idx.data()[GetTileElementOffset<TileSrc1Idx>(r, 0)];
+        return {cols0, cols1};
+    }
+};
+
+template <typename TileDstIdx, typename TileSrc0Idx, typename TileSrc1Idx>
+struct TConCatDstIdxCols {
+    TileDstIdx &dstIdx;
+    TileSrc0Idx &src0Idx;
+    TileSrc1Idx &src1Idx;
+
+    PTO_INTERNAL std::pair<unsigned, unsigned> GetColsNumber(unsigned r)
+    {
+        unsigned cols0 = src0Idx.data()[GetTileElementOffset<TileSrc0Idx>(r, 0)];
+        unsigned cols1 = src1Idx.data()[GetTileElementOffset<TileSrc1Idx>(r, 0)];
+        dstIdx.data()[GetTileElementOffset<TileDstIdx>(r, 0)] = cols0 + cols1;
+        return {cols0, cols1};
+    }
+};
+
+template <typename TileDst, typename TileSrc0, typename TileSrc1>
+PTO_INTERNAL void TCONCAT_IMPL(TileDst &dst, TileSrc0 &src0, TileSrc1 &src1)
+{
+    TConcatTileCheck(dst, src0, src1);
+    const unsigned cols0 = src0.GetValidCol();
+    const unsigned cols1 = src1.GetValidCol();
+    TConcatProcessor(dst, src0, src1, TConCatCols{cols0, cols1});
+}
+
+template <typename TileDst, typename TileSrc0, typename TileSrc1, typename TileSrc0Idx, typename TileSrc1Idx>
+PTO_INTERNAL void TCONCAT_IMPL(TileDst &dst, TileSrc0 &src0, TileSrc1 &src1, TileSrc0Idx &src0Idx, TileSrc1Idx &src1Idx)
+{
+    TConcatIdxTileCheck(dst, src0, src1, src0Idx, src1Idx);
+    TConcatProcessor(dst, src0, src1, TConCatIdxCols{src0Idx, src1Idx});
+}
+
+template <typename TileDst, typename TileSrc0, typename TileSrc1, typename TileDstIdx, typename TileSrc0Idx,
+          typename TileSrc1Idx>
+PTO_INTERNAL void TCONCAT_IMPL(TileDst &dst, TileSrc0 &src0, TileSrc1 &src1, TileDstIdx &dstIdx, TileSrc0Idx &src0Idx,
+                               TileSrc1Idx &src1Idx)
+{
+    TConcatIdxTileCheck(dst, src0, src1, dstIdx, src0Idx, src1Idx);
+    TConcatProcessor(dst, src0, src1, TConCatDstIdxCols{dstIdx, src0Idx, src1Idx});
 }
 } // namespace pto
 
--- tests/cpu/st/testcase/tconcat/gen_data.py
+++ tests/cpu/st/testcase/tconcat/gen_data.py
@@ -22,6 +22,13 @@ def gen_golden_data(case_name, param):
     src0_tile_row, src0_tile_col = param.src0_tile_row, param.src0_tile_col
     src1_tile_row, src1_tile_col = param.src1_tile_row, param.src1_tile_col
     v_valid_row, v_valid_col0, v_valid_col1 = param.valid_row, param.valid_col0, param.valid_col1
+    idx_tiles = param.idx_tiles
+    if (idx_tiles != 'none'):
+        idxtype = param.idxtype
+        src0_idx_row, src0_idx_col = param.src0_idx_row, param.src0_idx_col
+        src1_idx_row, src1_idx_col = param.src1_idx_row, param.src1_idx_col
+    if (idx_tiles == 'dst'):
+        dst_idx_row, dst_idx_col = param.dst_idx_row, param.dst_idx_col
 
     # Generate input arrays
     input1_valid = np.random.uniform(-1000, 1000, size=(v_valid_row, v_valid_col0)).astype(dtype)
@@ -30,43 +37,94 @@ def gen_golden_data(case_name, param):
     input2 = np.zeros([src1_tile_row, src1_tile_col]).astype(dtype)
     input1[0:v_valid_row, 0:v_valid_col0] = input1_valid
     input2[0:v_valid_row, 0:v_valid_col1] = input2_valid
+    if (idx_tiles != 'none'):
+        input1_idx_valid = np.random.uniform(0, v_valid_col0, size=(v_valid_row, 1)).astype(idxtype)
+        input2_idx_valid = np.random.uniform(0, v_valid_col1, size=(v_valid_row, 1)).astype(idxtype)
+        input1_idx = np.zeros([src0_idx_row, src0_idx_col]).astype(idxtype)
+        input2_idx = np.zeros([src1_idx_row, src1_idx_col]).astype(idxtype)
+        input1_idx[0:v_valid_row, 0:1] = input1_idx_valid
+        input2_idx[0:v_valid_row, 0:1] = input2_idx_valid
 
     # Perform the concat operation
     golden = np.zeros([dst_tile_row, dst_tile_col]).astype(dtype)
-    golden[0:v_valid_row, 0:v_valid_col0] = input1[0:v_valid_row, 0:v_valid_col0]
-    golden[0:v_valid_row, v_valid_col0:v_valid_col0 + v_valid_col1] = input2[0:v_valid_row, 0:v_valid_col1]
+    if (idx_tiles == 'dst'):
+        golden_idx = np.zeros([dst_idx_row, dst_idx_col]).astype(idxtype)
+
+    if (idx_tiles == 'none'):
+        golden[:v_valid_row, :v_valid_col0 + v_valid_col1] = np.concatenate([
+            input1[:v_valid_row, :v_valid_col0],
+            input2[:v_valid_row, :v_valid_col1]
+        ], axis=1)
+    else:
+        if (idx_tiles == 'dst'):
+            golden_idx[:v_valid_row, :1] = (input1_idx + input2_idx)[:v_valid_row, :1]
+        for i in range(v_valid_row):
+            col1_len = input1_idx[i, 0]
+            col2_len = input2_idx[i, 0]
+
+            golden[i, :col1_len] = input1[i, :col1_len]
+            golden[i, col1_len:col1_len + col2_len] = input2[i, :col2_len]
+
 
     # Save the input and golden data to binary files
     input1.tofile("input1.bin")
     input2.tofile("input2.bin")
     golden.tofile("golden.bin")
+    if (idx_tiles != 'none'):
+        input1_idx.tofile("input1_idx.bin")
+        input2_idx.tofile("input2_idx.bin")
+    if (idx_tiles == 'dst'):
+        golden_idx.tofile("golden_idx.bin")
 
 
 class TConcatParams:
-    def __init__(self, dtype, dst_h, dst_w, src0_h, src0_w, src1_h, src1_w, valid_row, valid_col0, valid_col1):
+    def __init__(self, dtype, valid_row, valid_col0, valid_col1, idxtype=None, idx_tiles='none', dst_h=None,
+                 dst_w=None, src0_h=None, src0_w=None, src1_h=None, src1_w=None, dst_idx_h=None, dst_idx_w=None,
+                 src0_idx_h=None, src0_idx_w=None, src1_idx_h=None, src1_idx_w=None):
         self.dtype = dtype
-        self.dst_tile_row = dst_h
-        self.dst_tile_col = dst_w
-        self.src0_tile_row = src0_h
-        self.src0_tile_col = src0_w
-        self.src1_tile_row = src1_h
-        self.src1_tile_col = src1_w
+        self.idxtype = idxtype
+        self.idx_tiles = idx_tiles
         self.valid_row = valid_row
         self.valid_col0 = valid_col0
         self.valid_col1 = valid_col1
+        self.dst_tile_row = self._default_if_none(dst_h, valid_row)
+        self.dst_tile_col = self._default_if_none(dst_w, valid_col0 + valid_col1)
+        self.src0_tile_row = self._default_if_none(src0_h, valid_row)
+        self.src0_tile_col = self._default_if_none(src0_w, valid_col0)
+        self.src1_tile_row = self._default_if_none(src1_h, valid_row)
+        self.src1_tile_col = self._default_if_none(src1_w, valid_col1)
+
+        self.dst_idx_row = self._default_if_none(dst_idx_h, self.dst_tile_row)
+        self.dst_idx_col = self._default_if_none(dst_idx_w, self.dst_tile_col)
+        self.src0_idx_row = self._default_if_none(src0_idx_h, self.src0_tile_row)
+        self.src0_idx_col = self._default_if_none(src0_idx_w, self.src0_tile_col)
+        self.src1_idx_row = self._default_if_none(src1_idx_h, self.src1_tile_row)
+        self.src1_idx_col = self._default_if_none(src1_idx_w, self.src1_tile_col)
+
+    @staticmethod
+    def _default_if_none(value, default):
+        return default if value is None else value
 
 
 def generate_case_name(param):
-    dtype_str = {
+    types_dict = {
         np.float32: 'float',
         np.float16: 'half',
         np.int8: 'int8',
         np.int32: 'int32',
         np.int16: 'int16'
-    }[param.dtype]
-    return f"TCONCATTest.case_{dtype_str}_{param.dst_tile_row}x{param.dst_tile_col}_\
-{param.src0_tile_row}x{param.src0_tile_col}_{param.src1_tile_row}x{param.src1_tile_col}_\
-{param.valid_row}x{param.valid_col0}_{param.valid_row}x{param.valid_col1}"
+    }
+    dtype_str = types_dict.get(param.dtype)
+    idxtype_str = types_dict.get(param.idxtype)
+    idx_tiles = param.idx_tiles
+    sizes_str = f"{param.dst_tile_row}x{param.dst_tile_col}_{param.src0_tile_row}x{param.src0_tile_col}_\
+{param.src1_tile_row}x{param.src1_tile_col}_{param.valid_row}x{param.valid_col0}_{param.valid_row}x{param.valid_col1}"
+    idxsizes_str = f"{param.dst_idx_row}x{param.dst_idx_col}_{param.src0_idx_row}x{param.src0_idx_col}_\
+{param.src1_idx_row}x{param.src1_idx_col}"
+
+    if (idx_tiles == 'none'):
+        return f"TCONCATTest.case_{dtype_str}_{sizes_str}"
+    return f"TCONCATTest.case_{dtype_str}_{idxtype_str}_{idx_tiles}_{sizes_str}_{idxsizes_str}"
 
 
 if __name__ == "__main__":
@@ -79,14 +137,30 @@ if __name__ == "__main__":
         os.makedirs(testcases_dir)
 
     case_params_list = [
-        TConcatParams(np.float32, 64, 128, 64, 64, 64, 64, 64, 64, 64),
-        TConcatParams(np.int32, 64, 128, 64, 64, 64, 64, 64, 64, 64),
-        TConcatParams(np.float16, 16, 256, 16, 128, 16, 128, 16, 128, 128),
-        TConcatParams(np.float32, 16, 64, 16, 32, 16, 32, 16, 32, 32),
-        TConcatParams(np.int16, 32, 256, 32, 128, 32, 128, 32, 128, 128),
-        TConcatParams(np.float16, 16, 128, 16, 64, 16, 64, 16, 63, 64),
-        TConcatParams(np.float32, 16, 64, 16, 32, 16, 32, 16, 31, 32),
-        TConcatParams(np.int16, 32, 256, 32, 128, 32, 128, 32, 127, 128),
+        TConcatParams(np.float32, 64, 64, 64),
+        TConcatParams(np.int32, 64, 64, 64),
+        TConcatParams(np.float16, 16, 128, 128),
+        TConcatParams(np.float32, 16, 32, 32),
+        TConcatParams(np.int16, 32, 128, 128),
+        TConcatParams(np.float16, 16, 63, 64, src0_w=64, dst_w=128),
+        TConcatParams(np.float32, 16, 31, 32, src0_w=32, dst_w=64),
+        TConcatParams(np.int16, 32, 127, 128, src0_w=128, dst_w=256),
+        TConcatParams(np.float32, 64, 64, 64, np.int32, 'src'),
+        TConcatParams(np.int32, 64, 64, 64, np.int32, 'src'),
+        TConcatParams(np.float16, 16, 128, 128, np.int32, 'src'),
+        TConcatParams(np.float32, 16, 32, 32, np.int32, 'src'),
+        TConcatParams(np.int16, 32, 128, 128, np.int32, 'src'),
+        TConcatParams(np.float16, 16, 63, 64, np.int32, 'src', src0_w=64, dst_w=128),
+        TConcatParams(np.float32, 16, 31, 32, np.int32, 'src', src0_w=32, dst_w=64),
+        TConcatParams(np.int16, 32, 127, 128, np.int32, 'src', src0_w=128, dst_w=256),
+        TConcatParams(np.float32, 64, 64, 64, np.int32, 'dst'),
+        TConcatParams(np.int32, 64, 64, 64, np.int32, 'dst'),
+        TConcatParams(np.float16, 16, 128, 128, np.int32, 'dst'),
+        TConcatParams(np.float32, 16, 32, 32, np.int32, 'dst'),
+        TConcatParams(np.int16, 32, 128, 128, np.int32, 'dst'),
+        TConcatParams(np.float16, 16, 63, 64, np.int32, 'dst', src0_w=64, dst_w=128),
+        TConcatParams(np.float32, 16, 31, 32, np.int32, 'dst', src0_w=32, dst_w=64),
+        TConcatParams(np.int16, 32, 127, 128, np.int32, 'dst', src0_w=128, dst_w=256),
     ]
 
     for param in case_params_list:
