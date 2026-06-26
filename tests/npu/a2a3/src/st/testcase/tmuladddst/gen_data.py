Fix A5 TFUSEDMULADD TFUSEDMULADDRELU TMULADDDST precision error
Co-authored-by: denglei0610<denglei31@h-partners.com>



# message auto-generated for no-merge-commit merge:
!1190 merge fix into master

Fix A5 TFUSEDMULADD TFUSEDMULADDRELU TMULADDDST precision error

Created-by: denglei0610
Commit-by: denglei0610
Merged-by: cann-robot
Description: ## 描述
<!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
A5 TFUSEDMULADD TFUSEDMULADDRELU TMULADDDST精度问题修复

## 关联的Issue
<!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
<!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
https://gitcode.com/cann/pto-isa/issues/442

## 测试
<!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->

## 文档更新
<!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->

## 类型标签
<!--  [x] 表示选中 -->
- [x] Bug修复
- [ ] 新特性
- [ ] 性能优化
- [ ] 文档更新
- [ ] 其他，请描述：




See merge request: cann/pto-isa!1190

tests/npu/a2a3/src/st/testcase/tmuladddst/gen_data.py
@@ -12,6 +12,14 @@
 
 import os
 import numpy as np
+import math
+
+
+def check_golden_data(golden, threshold=0.1):
+    total = golden.size
+    infcnt = np.sum(np.isinf(golden))
+    if float(infcnt) / float(total) > threshold:
+        raise ValueError('Too many inf value, please check golden generation.')
 
 
 def gen_golden_data(case_name, param):
@@ -24,13 +32,13 @@ def gen_golden_data(case_name, param):
     # Generate random input arrays
     if dtype in (np.int8, np.uint8, np.int16, np.uint16, np.int32, np.uint32):
         dtype_info = np.iinfo(dtype)
-        vmin, vmax = dtype_info.min / 100, dtype_info.max / 100
+        vmin, vmax = -math.sqrt(math.fabs(dtype_info.min)) / 2, math.sqrt(math.fabs(dtype_info.max)) / 2
         input0 = np.random.randint(vmin, vmax, size=[src0_tile_row, src0_tile_col]).astype(dtype)
         input1 = np.random.randint(vmin, vmax, size=[src1_tile_row, src1_tile_col]).astype(dtype)
         dst = np.random.randint(vmin, vmax, size=[dst_tile_row, dst_tile_col]).astype(dtype)
     else:
         dtype_info = np.finfo(dtype)
-        vmin, vmax = dtype_info.min / 100, dtype_info.max / 100
+        vmin, vmax = -math.sqrt(math.fabs(dtype_info.min)) / 10, math.sqrt(math.fabs(dtype_info.max)) / 10
         input0 = np.random.uniform(low=vmin, high=vmax, size=[src0_tile_row, src0_tile_col]).astype(dtype)
         input1 = np.random.uniform(low=vmin, high=vmax, size=[src1_tile_row, src1_tile_col]).astype(dtype)
         dst = np.random.uniform(low=vmin, high=vmax, size=[dst_tile_row, dst_tile_col]).astype(dtype)
@@ -42,6 +50,7 @@ def gen_golden_data(case_name, param):
     # Perform the operation
     dst[0:h_valid, 0:w_valid] = input0[0:h_valid, 0:w_valid] * input1[0:h_valid, 0:w_valid] +\
         dst[0:h_valid, 0:w_valid]
+    check_golden_data(dst)
 
     # Save the input and golden data to binary files
     dst.tofile("golden.bin")