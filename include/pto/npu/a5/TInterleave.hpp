873508aa96b2181d619f6714dd4af79c0b65bab5
csjlchen <chenjinlin1@huawei.com>
2026-07-15 18:35:25 +0800

    Update TInsert and TInterleave.
    Co-authored-by: chen jinlin<chenjinlin1@huawei.com>
    
    # message auto-generated for no-merge-commit merge:
    !1328 merge update_tintlv into master
    
    Update TInsert and TInterleave.
    
    Created-by: csjlchen
    Commit-by: chen jinlin
    Merged-by: cann-robot
    Description: ## 描述
    Update TInsert and TInterleave.
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    All the pypto testcases are passed.
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    See merge request: cann/pto-isa!1328
    
    (cherry picked from commit 4de3c91f681529c4a283bd36dc4ca80577027ea9)

--- include/pto/npu/a5/TInterleave.hpp
+++ include/pto/npu/a5/TInterleave.hpp
@@ -50,6 +50,64 @@ PTO_INTERNAL void TInterleaveCheck(
         "Fix: TINTERLEAVE output tile dst1 valid shape mismatch with output tile dst0 shape.");
 }
 
+template <unsigned ElementsPerRepeat>
+PTO_INTERNAL void TInterleaveCalcMasks(uint32_t validCols, uint16_t j, uint32_t& lowerMask, uint32_t& upperMask)
+{
+    uint32_t remaining = validCols - static_cast<uint32_t>(j) * 2 * ElementsPerRepeat;
+    lowerMask = remaining > ElementsPerRepeat ? ElementsPerRepeat : remaining;
+    upperMask = remaining > ElementsPerRepeat ? remaining - ElementsPerRepeat : 0;
+    upperMask = upperMask > ElementsPerRepeat ? ElementsPerRepeat : upperMask;
+}
+
+template <typename T, unsigned ElementsPerRepeat, typename DistValue>
+PTO_INTERNAL void TInterleaveStoreSplit(
+    RegTensor<T>& dst0Reg, RegTensor<T>& dst1Reg, __ubuf__ T* dstPtr, uint32_t dstOffset, DistValue distValue,
+    uint32_t lowerMask, uint32_t upperMask)
+{
+    MaskReg preg = CreatePredicate<T>(lowerMask);
+    vsts(dst0Reg, dstPtr, dstOffset, distValue, preg);
+    if (upperMask > 0) {
+        preg = CreatePredicate<T>(upperMask);
+        vsts(dst1Reg, dstPtr, dstOffset + ElementsPerRepeat, distValue, preg);
+    }
+}
+
+template <typename T, unsigned ElementsPerRepeat, typename DistValue>
+PTO_INTERNAL void TInterleaveAlignedHalf(
+    RegTensor<T>& src0Reg, RegTensor<T>& src1Reg, RegTensor<T>& dst0Reg, RegTensor<T>& dst1Reg, __ubuf__ T* dstPtr,
+    __ubuf__ T* src0Ptr, __ubuf__ T* src1Ptr, uint32_t srcBase, uint32_t dstBase, uint16_t repeatTime,
+    uint32_t validCols, DistValue distValue)
+{
+    uint32_t lowerMask, upperMask;
+    for (uint16_t j = 0; j < repeatTime; ++j) {
+        TInterleaveCalcMasks<ElementsPerRepeat>(validCols, j, lowerMask, upperMask);
+        vlds(src0Reg, src0Ptr, srcBase + j * ElementsPerRepeat, NORM);
+        vlds(src1Reg, src1Ptr, srcBase + j * ElementsPerRepeat, NORM);
+        vintlv(dst0Reg, dst1Reg, src0Reg, src1Reg);
+        TInterleaveStoreSplit<T, ElementsPerRepeat>(
+            dst0Reg, dst1Reg, dstPtr, dstBase + j * 2 * ElementsPerRepeat, distValue, lowerMask, upperMask);
+    }
+}
+
+template <typename T, unsigned ElementsPerRepeat, typename DistValue>
+PTO_INTERNAL void TInterleaveUnalignedHalf(
+    RegTensor<T>& src0Reg, RegTensor<T>& src1Reg, RegTensor<T>& dst0Reg, RegTensor<T>& dst1Reg, UnalignReg& ureg,
+    __ubuf__ T* dstPtr, __ubuf__ T* src0Ptr, __ubuf__ T* src1Ptr, uint32_t srcBase, uint32_t dstBase,
+    uint16_t repeatTime, uint32_t validCols, DistValue distValue)
+{
+    uint32_t lowerMask, upperMask;
+    for (uint16_t j = 0; j < repeatTime; ++j) {
+        TInterleaveCalcMasks<ElementsPerRepeat>(validCols, j, lowerMask, upperMask);
+        vldas(ureg, src0Ptr + srcBase + j * ElementsPerRepeat);
+        vldus(src0Reg, ureg, src0Ptr + srcBase + j * ElementsPerRepeat);
+        vldas(ureg, src1Ptr + srcBase + j * ElementsPerRepeat);
+        vldus(src1Reg, ureg, src1Ptr + srcBase + j * ElementsPerRepeat);
+        vintlv(dst0Reg, dst1Reg, src0Reg, src1Reg);
+        TInterleaveStoreSplit<T, ElementsPerRepeat>(
+            dst0Reg, dst1Reg, dstPtr, dstBase + j * 2 * ElementsPerRepeat, distValue, lowerMask, upperMask);
+    }
+}
+
 template <typename TileDataDst, typename TileDataSrc, unsigned ElementsPerRepeat, unsigned BlockSizeElem>
 __tf__ PTO_INTERNAL void TInterleaveAlign(
     typename TileDataDst::TileDType __out__ dst1, typename TileDataDst::TileDType __out__ dst0,
@@ -72,37 +130,16 @@ __tf__ PTO_INTERNAL void TInterleaveAlign(
     __VEC_SCOPE__
     {
         RegTensor<T> src0Reg, src1Reg, dst0Reg, dst1Reg;
-        MaskReg preg;
         for (uint16_t i = 0; i < (uint16_t)validRows; ++i) {
-            uint32_t sreg = validCols;
-            // Interleave src0(first half) and src1(first half) into dst0
-            for (uint16_t j = 0; j < repeatTime; ++j) {
-                vlds(src0Reg, src0Ptr, i * srcRowStride + j * ElementsPerRepeat, NORM);
-                vlds(src1Reg, src1Ptr, i * srcRowStride + j * ElementsPerRepeat, NORM);
-                vintlv(dst0Reg, dst1Reg, src0Reg, src1Reg);
-                preg = CreatePredicate<T>(sreg);
-                vsts(dst0Reg, dst0Ptr, i * dstRowStride + j * 2 * ElementsPerRepeat, distValue, preg);
-                preg = CreatePredicate<T>(sreg);
-                vsts(
-                    dst1Reg, dst0Ptr, i * dstRowStride + j * 2 * ElementsPerRepeat + ElementsPerRepeat, distValue,
-                    preg);
-            }
-            // Interleave src0(second half) and src1(second half) into dst1
-            sreg = validCols;
-            // validCols is block aligned, so we can use aligned load
-            for (uint16_t j = 0; j < repeatTime; ++j) {
-                vlds(src0Reg, src0Ptr, i * srcRowStride + halfValidCols + j * ElementsPerRepeat, NORM);
-                vlds(src1Reg, src1Ptr, i * srcRowStride + halfValidCols + j * ElementsPerRepeat, NORM);
-                vintlv(dst0Reg, dst1Reg, src0Reg, src1Reg);
-                preg = CreatePredicate<T>(sreg);
-                vsts(dst0Reg, dst1Ptr, i * dstRowStride + j * 2 * ElementsPerRepeat, distValue, preg);
-                preg = CreatePredicate<T>(sreg);
-                vsts(
-                    dst1Reg, dst1Ptr, i * dstRowStride + j * 2 * ElementsPerRepeat + ElementsPerRepeat, distValue,
-                    preg);
-            }
-        } // end loop i
-    } // end vec scope
+            // Interleave src0/src1 first half into dst0; second half into dst1 (aligned loads)
+            TInterleaveAlignedHalf<T, ElementsPerRepeat>(
+                src0Reg, src1Reg, dst0Reg, dst1Reg, dst0Ptr, src0Ptr, src1Ptr, i * srcRowStride, i * dstRowStride,
+                repeatTime, validCols, distValue);
+            TInterleaveAlignedHalf<T, ElementsPerRepeat>(
+                src0Reg, src1Reg, dst0Reg, dst1Reg, dst1Ptr, src0Ptr, src1Ptr, i * srcRowStride + halfValidCols,
+                i * dstRowStride, repeatTime, validCols, distValue);
+        }
+    }
 }
 
 template <typename TileDataDst, typename TileDataSrc, unsigned ElementsPerRepeat, unsigned BlockSizeElem>
@@ -126,42 +163,18 @@ __tf__ PTO_INTERNAL void TInterleaveUnalign(
 
     __VEC_SCOPE__
     {
-        MaskReg preg;
         UnalignReg ureg;
         RegTensor<T> src0Reg, src1Reg, dst0Reg, dst1Reg;
         for (uint16_t i = 0; i < (uint16_t)validRows; ++i) {
-            // Interleave src0(first half) and src1(first half) into dst0
-            uint32_t sreg = validCols;
-            for (uint16_t j = 0; j < repeatTime; ++j) {
-                vlds(src0Reg, src0Ptr, i * srcRowStride + j * ElementsPerRepeat, NORM);
-                vlds(src1Reg, src1Ptr, i * srcRowStride + j * ElementsPerRepeat, NORM);
-                vintlv(dst0Reg, dst1Reg, src0Reg, src1Reg);
-                preg = CreatePredicate<T>(sreg);
-                vsts(dst0Reg, dst0Ptr, i * dstRowStride + j * 2 * ElementsPerRepeat, distValue, preg);
-                preg = CreatePredicate<T>(sreg);
-                vsts(
-                    dst1Reg, dst0Ptr, i * dstRowStride + j * 2 * ElementsPerRepeat + ElementsPerRepeat, distValue,
-                    preg);
-            }
-
-            // Interleave src0(second half) and src1(second half) into dst1
-            sreg = validCols;
-            // validCols is block unaligned, so we use unaligned load
-            for (uint16_t j = 0; j < repeatTime; ++j) {
-                vldas(ureg, src0Ptr + i * srcRowStride + halfValidCols + j * ElementsPerRepeat);
-                vldus(src0Reg, ureg, src0Ptr + i * srcRowStride + halfValidCols + j * ElementsPerRepeat);
-                vldas(ureg, src1Ptr + i * srcRowStride + halfValidCols + j * ElementsPerRepeat);
-                vldus(src1Reg, ureg, src1Ptr + i * srcRowStride + halfValidCols + j * ElementsPerRepeat);
-                vintlv(dst0Reg, dst1Reg, src0Reg, src1Reg);
-                preg = CreatePredicate<T>(sreg);
-                vsts(dst0Reg, dst1Ptr, i * dstRowStride + j * 2 * ElementsPerRepeat, distValue, preg);
-                preg = CreatePredicate<T>(sreg);
-                vsts(
-                    dst1Reg, dst1Ptr, i * dstRowStride + j * 2 * ElementsPerRepeat + ElementsPerRepeat, distValue,
-                    preg);
-            }
-        } // end loop i
-    } // end vec scope
+            // First half: aligned loads into dst0; second half: unaligned loads into dst1
+            TInterleaveAlignedHalf<T, ElementsPerRepeat>(
+                src0Reg, src1Reg, dst0Reg, dst1Reg, dst0Ptr, src0Ptr, src1Ptr, i * srcRowStride, i * dstRowStride,
+                repeatTime, validCols, distValue);
+            TInterleaveUnalignedHalf<T, ElementsPerRepeat>(
+                src0Reg, src1Reg, dst0Reg, dst1Reg, ureg, dst1Ptr, src0Ptr, src1Ptr, i * srcRowStride + halfValidCols,
+                i * dstRowStride, repeatTime, validCols, distValue);
+        }
+    }
 }
 
 template <typename TileDataDst, typename TileDataSrc>
@@ -170,7 +183,7 @@ PTO_INTERNAL void TINTERLEAVE_IMPL(TileDataDst& dst1, TileDataDst& dst0, TileDat
     using T = typename TileDataDst::DType;
     TInterleaveCheck<TileDataDst, TileDataSrc>(dst1, dst0, src1, src0);
     constexpr unsigned blockSizeElem = BLOCK_BYTE_SIZE / sizeof(T);
-    constexpr unsigned elementsPerRepeat = CCE_VL / sizeof(T);
+    constexpr unsigned elementsPerRepeat = REPEAT_BYTE / sizeof(T);
 
     const bool isValicColAlign = ((dst0.GetValidCol() * sizeof(T) >> 1) % BLOCK_BYTE_SIZE == 0);
     if (isValicColAlign) {
