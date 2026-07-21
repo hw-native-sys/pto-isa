1ee04d3ff39c337be8060618cb1c747ea8b7d871
Azizbek <habibov.azizbek@h-partners.com>
2026-07-21 21:00:59 +0800

    [CPU-SIM] Fix npu warning for IsTwinType
    Co-authored-by: Azizbek<habibov.azizbek@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1360 merge wr into master
    
    [CPU-SIM] Fix npu warning for IsTwinType
    
    Created-by: Azizbek
    Commit-by: Azizbek
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/pto-isa!1360

--- include/pto/common/pto_tile.hpp
+++ include/pto/common/pto_tile.hpp
@@ -1577,6 +1577,7 @@ public:
     PTO_INTERNAL void ResetMadMode() { set_ctrl(sbitset0(get_ctrl(), MAD_MODE_BIT)); }
 #endif
 
+#if defined(__CPU_SIM)
     static constexpr size_t GetSizeInUnits()
     {
         // One unit is sizeof(DType)
@@ -1589,7 +1590,6 @@ public:
 
     static constexpr size_t GetSizeInBytes() { return GetSizeInUnits() * sizeof(DType); }
 
-#if defined(__CPU_SIM)
     DType GetElement(int64_t r, int64_t c)
     {
         return GetProperDataPart(data(), GetTileElementOffset<std::remove_reference_t<decltype(*this)>>(r, c));
