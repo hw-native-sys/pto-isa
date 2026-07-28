hw-native-sys/pto-isa

commit 021789c0b32f5cd7be9c1990f66f85e3853e7888

refactor: extract Event CRTP base class and replace opPipeList with OpPipeEntry template
Co-authored-by: HuangXingYuan_777<huangxingyuan4@huawei.com>

!1160 merge TSYNC into master

refactor: extract Event CRTP base class and replace opPipeList with OpPipeEntry template

Created-by: HuangXingYuan_777
Commit-by: HuangXingYuan_777
Merged-by: cann-robot
Description: ## 描述
- Add EventBase CRTP base class in event.hpp with common Event logic: Wait/Init/Record/InitAddr dispatch, token management, constructors, self() helper
- Replace opPipeList[] array with type-safe OpPipeEntry template specialization
- Unify is_event/all_events_v traits via EventBaseTag + std::is_base_of
- Refactor platform Event classes (a5/a2a3/kirin9030) to inherit EventBase:
- a5: cross-core intra_block sync, isValidBarrierPipe (MTE2/MTE3)
- a2a3: FFTS cross-core sync, expanded TSYNC pipeline support
- kirin9030: barrier-based sync, no cross-core
- Fix TPOW/TPOWS visibility (PTO_INTERNAL -> PTO_INST)
- Guard TPUSH/TFREE with PTO_COMM_NOT_SUPPORTED
- Add missing includes: TBinOp.hpp, SetQuantScalar/Vector.hpp
- Update test cases to use Event-based API

<!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
<!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->

<!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->

<!--  [x] 表示选中 -->
- [ ] Bug修复
- [x] 新特性
- [ ] 性能优化
- [ ] 文档更新
- [ ] 其他，请描述：

See merge request: cann/pto-isa!1160

include/pto/common/event.hpp
@@ -93,7 +93,6 @@ enum class Op : uint16_t
     TSTORE_MAT,   /* Mat to GM */
     TMATMUL,
     TGEMV,
-    TMATMUL_MX,
     TEXTRACT_M2LR, /* Mat to Left/Right */
     TANDS,
     TORS,
@@ -110,104 +109,203 @@ enum class Op : uint16_t
     TCONCAT,
     TDEQUANT,
     TADDDEQRELU,
+    TABS,
+    TNEG,
+    TRELU,
+    TNOT,
+    TLOG,
+    TRECIP,
+    TCMP,
+    TSCATTER,
+    TCOLEXPAND,
+    TCOLEXPANDDIV,
+    TCOLEXPANDMUL,
+    TCOLEXPANDADD,
+    TCOLEXPANDMAX,
+    TCOLEXPANDMIN,
+    TCOLEXPANDSUB,
+    TCOLEXPANDEXPDIF,
+    TROWEXPANDDIV,
+    TROWEXPANDMUL,
+    TROWEXPANDSUB,
+    TROWEXPANDADD,
+    TROWEXPANDMAX,
+    TROWEXPANDMIN,
+    TROWEXPANDEXPDIF,
+    TPAIRREDUCESUM,
+    TSUBRELUCONV,
+    TADDRELUCONV,
+    TFUSEDMULADD,
+    TMULADDDST,
+    TSUBRELU,
+    TFUSEDMULADDRELU,
+    TPARTARGMAX,
+    TPARTARGMIN,
+    TCOLARGMAX,
+    TCOLARGMIN,
+    TROWARGMAX,
+    TROWARGMIN,
+    TPREFETCH,
+    TFILLPAD_VEC,
+    TFILLPAD_MAT,
+    MGATHER_VEC,
+    MGATHER_MAT,
+    MSCATTER,
+    TQUANT,
+    THISTOGRAM,
+    TINTERLEAVE,
+    TDEINTERLEAVE,
     OP_COUNT, // The Total number of operations, please add new operations before OP_COUNT
 };
 
-// opPipeList maps each operation in Op enum to its corresponding pipeline type.
-// This array is used to determine which hardware pipeline should be used for each operation.
-constexpr pipe_t opPipeList[] = {
-    PIPE_MTE2 /* TLOAD */,
-    PIPE_MTE3 /* TSTORE_VEC */,
-    PIPE_S /* SCALAR */,
-    PIPE_S /* TRESHAPE */,
-    PIPE_V /* VECTOR */,
-    PIPE_V /* TADD */,
-    PIPE_V /* TADDS */,
-    PIPE_V /* TAXPY */,
-    PIPE_V /* TSUB */,
-    PIPE_V /* TMUL */,
-    PIPE_V /* TMULS */,
-    PIPE_V /* TDIV */,
-    PIPE_V /* TDIVS */,
-    PIPE_V /* TMIN */,
-    PIPE_V /* TMINS */,
-    PIPE_V /* TMAX */,
-    PIPE_V /* TAND */,
-    PIPE_V /* TOR */,
-    PIPE_V /* TSEL */,
-    PIPE_V /* TSHL */,
-    PIPE_V /* TSHR */,
-    PIPE_V /* TEXP */,
-    PIPE_V /* TSELS */,
-    PIPE_V /* TSQRT */,
-    PIPE_V /* TRSQRT */,
-    PIPE_V /* TEXPANDS */,
-    PIPE_MTE2 /* TEXPANDS_MAT */,
-    PIPE_V /* TPARTADD */,
-    PIPE_V /* TPARTMUL */,
-    PIPE_V /* TPARTMAX */,
-    PIPE_V /* TPARTMIN */,
-    PIPE_V /* TPOW */,
-    PIPE_V /* TPOWS */,
-    PIPE_V /* TCMPS */,
-    PIPE_V /* TMRGSORT */,
-    PIPE_V /* TSORT32 */,
-    PIPE_S /* TCI */,
-    PIPE_V /* TGATHER */,
-    PIPE_V /* TGATHERB */,
-    PIPE_V /* TCVT */,
-    PIPE_V /* TROWSUM */,
-    PIPE_V /* TROWPROD */,
-    PIPE_V /* TROWMAX */,
-    PIPE_V /* TROWMIN */,
-    PIPE_V /* TROWEXPAND */,
-    PIPE_V /* TRANDOM */,
-    PIPE_V /* TCOLSUM */,
-    PIPE_V /* TCOLPROD */,
-    PIPE_V /* TCOLMAX */,
-    PIPE_V /* TCOLMIN */,
-    PIPE_V /* TTRANS */,
-    PIPE_V /* TTRI */,
-    PIPE_V /* TREM */,
-    PIPE_V /* TFMOD */,
-    PIPE_V /* TREMS */,
-    PIPE_V /* TFMODS */,
-    PIPE_V /* TSUBS */,
-    PIPE_V /* TMAXS */,
-    PIPE_V /* TLRELU */,
-    PIPE_V /* TPRELU */,
-    PIPE_V /* TMOV_V2V */,
-    PIPE_FIX /* TMOV_V2M */,
-    PIPE_FIX /* TEXTRACT_V2M */,
-    PIPE_MTE1 /* TMOV_M2B */,
-    PIPE_MTE1 /* TMOV_M2L */,
-    PIPE_MTE1 /* TMOV_M2R */,
-    PIPE_FIX /* TMOV_M2S */,
-    PIPE_FIX /* TMOV_A2V */,
-    PIPE_FIX /* TMOV_A2M */,
-    PIPE_FIX /* TSTORE_ACC */,
-    PIPE_MTE3 /* TSTORE_MAT */,
-    PIPE_M /* TMATMUL */,
-    PIPE_M /* TGEMV */,
-    PIPE_M /* TMATMUL_MX */,
-    PIPE_MTE1 /* TEXTRACT_M2LR */,
-    PIPE_V /* TANDS */,
-    PIPE_V /* TORS */,
-    PIPE_V /* TSHLS */,
-    PIPE_V /* TSHRS */,
-    PIPE_V /* TXOR */,
-    PIPE_V /* TXORS */,
-    PIPE_FIX /* TEXTRACT_A2M */,
-    PIPE_FIX /* TINSERT_A2M */,
-    PIPE_MTE1 /* TIMG2COL */,
-    PIPE_S /* SETFMATRIX */,
-    PIPE_S /* SET_IMG2COL_RPT */,
-    PIPE_S /* SET_IMG2COL_PADDING */,
-    PIPE_V /* TCONCAT */,
-    PIPE_V /* TDEQUANT */,
-    PIPE_V /* TADDDEQRELU */,
-    PIPE_ALL /* OP_COUNT */,
+template <Op Op_>
+struct OpPipeEntry {
+    static constexpr pipe_t pipe = PIPE_ALL;
 };
+
+#define PTO_DEFINE_OP_PIPE(Op_, Pipe_)        \
+    template <>                               \
+    struct OpPipeEntry<Op_> {                 \
+        static constexpr pipe_t pipe = Pipe_; \
+    }
+
+PTO_DEFINE_OP_PIPE(Op::TLOAD, PIPE_MTE2);
+PTO_DEFINE_OP_PIPE(Op::TSTORE_VEC, PIPE_MTE3);
+PTO_DEFINE_OP_PIPE(Op::SCALAR, PIPE_S);
+PTO_DEFINE_OP_PIPE(Op::TRESHAPE, PIPE_S);
+PTO_DEFINE_OP_PIPE(Op::VECTOR, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TADD, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TADDS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TAXPY, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSUB, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMUL, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMULS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TDIV, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TDIVS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMIN, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMINS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMAX, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TAND, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TOR, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSEL, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSHL, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSHR, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TEXP, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSELS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSQRT, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TRSQRT, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TEXPANDS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TEXPANDS_MAT, PIPE_MTE2);
+PTO_DEFINE_OP_PIPE(Op::TPARTADD, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPARTMUL, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPARTMAX, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPARTMIN, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPOW, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPOWS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCMPS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMRGSORT, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSORT32, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCI, PIPE_S);
+PTO_DEFINE_OP_PIPE(Op::TGATHER, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TGATHERB, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCVT, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWSUM, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWPROD, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWMAX, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWMIN, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWEXPAND, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TRANDOM, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLSUM, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLPROD, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLMAX, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLMIN, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TTRANS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TTRI, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TREM, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TFMOD, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TREMS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TFMODS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSUBS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMAXS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TLRELU, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPRELU, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMOV_V2V, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMOV_V2M, PIPE_FIX);
+PTO_DEFINE_OP_PIPE(Op::TEXTRACT_V2M, PIPE_FIX);
+PTO_DEFINE_OP_PIPE(Op::TMOV_M2B, PIPE_MTE1);
+PTO_DEFINE_OP_PIPE(Op::TMOV_M2L, PIPE_MTE1);
+PTO_DEFINE_OP_PIPE(Op::TMOV_M2R, PIPE_MTE1);
+PTO_DEFINE_OP_PIPE(Op::TMOV_M2S, PIPE_FIX);
+PTO_DEFINE_OP_PIPE(Op::TMOV_A2V, PIPE_FIX);
+PTO_DEFINE_OP_PIPE(Op::TMOV_A2M, PIPE_FIX);
+PTO_DEFINE_OP_PIPE(Op::TSTORE_ACC, PIPE_FIX);
+PTO_DEFINE_OP_PIPE(Op::TSTORE_MAT, PIPE_MTE3);
+PTO_DEFINE_OP_PIPE(Op::TMATMUL, PIPE_M);
+PTO_DEFINE_OP_PIPE(Op::TGEMV, PIPE_M);
+PTO_DEFINE_OP_PIPE(Op::TEXTRACT_M2LR, PIPE_MTE1);
+PTO_DEFINE_OP_PIPE(Op::TANDS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TORS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSHLS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSHRS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TXOR, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TXORS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TEXTRACT_A2M, PIPE_FIX);
+PTO_DEFINE_OP_PIPE(Op::TINSERT_A2M, PIPE_FIX);
+PTO_DEFINE_OP_PIPE(Op::TIMG2COL, PIPE_MTE1);
+PTO_DEFINE_OP_PIPE(Op::SETFMATRIX, PIPE_S);
+PTO_DEFINE_OP_PIPE(Op::SET_IMG2COL_RPT, PIPE_S);
+PTO_DEFINE_OP_PIPE(Op::SET_IMG2COL_PADDING, PIPE_S);
+PTO_DEFINE_OP_PIPE(Op::TCONCAT, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TDEQUANT, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TADDDEQRELU, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TABS, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TNEG, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TRELU, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TNOT, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TLOG, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TRECIP, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCMP, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSCATTER, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLEXPAND, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDDIV, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDMUL, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDADD, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDMAX, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDMIN, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDSUB, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDEXPDIF, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWEXPANDDIV, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWEXPANDMUL, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWEXPANDSUB, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWEXPANDADD, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWEXPANDMAX, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWEXPANDMIN, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWEXPANDEXPDIF, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPAIRREDUCESUM, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSUBRELUCONV, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TADDRELUCONV, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TFUSEDMULADD, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TMULADDDST, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TSUBRELU, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TFUSEDMULADDRELU, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPARTARGMAX, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPARTARGMIN, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLARGMAX, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLARGMIN, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWARGMAX, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TROWARGMIN, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TPREFETCH, PIPE_MTE2);
+PTO_DEFINE_OP_PIPE(Op::TFILLPAD_VEC, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TFILLPAD_MAT, PIPE_MTE2);
+PTO_DEFINE_OP_PIPE(Op::MGATHER_VEC, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::MGATHER_MAT, PIPE_MTE2);
+PTO_DEFINE_OP_PIPE(Op::MSCATTER, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TQUANT, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::THISTOGRAM, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TINTERLEAVE, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TDEINTERLEAVE, PIPE_V);
+
+#undef PTO_DEFINE_OP_PIPE
 
 struct RecordEvent {};
 
@@ -277,5 +375,85 @@ PTO_INTERNAL void PtoSetWaitFlag(event_t SetEventId = EVENT_ID0, event_t WaitEve
 #endif
 #endif
 }
+struct EventBaseTag {};
+
+template <typename T>
+struct is_event : std::is_base_of<EventBaseTag, T> {};
+
+template <typename... Ts>
+inline constexpr bool all_events_v = (is_event<Ts>::value && ...);
+
+// CRTP base class for platform-specific Event implementations.
+// Derived must provide:
+//   template <Op op> static constexpr pipe_t GetPipeByOp()
+//   static constexpr bool IsCrossCore
+//   template <uint8_t CrossCoreId> PTO_INTERNAL Derived& WaitImpl()
+//   template <uint8_t CrossCoreId> PTO_INTERNAL Derived& InitImpl()
+//   PTO_INTERNAL Derived& InitAddrImpl(uint64_t fftsAddr)
+template <typename Derived, Op SrcOp, Op DstOp, bool AutoToken = true, event_t EventID = EVENT_ID0>
+struct EventBase : EventBaseTag {
+#ifndef __PTO_AUTO__
+    static constexpr Op srcOp = SrcOp;
+    static constexpr Op dstOp = DstOp;
+    static constexpr pipe_t srcPipe = Derived::template GetPipeByOp<SrcOp>();
+    static constexpr pipe_t dstPipe = Derived::template GetPipeByOp<DstOp>();
+    static constexpr bool isSamePipe = (srcPipe == dstPipe);
+
+#ifdef PTO_FLAG_TEST
+    CceEventIdType token = {};
+#else
+    const event_t token = AutoToken ? EventIdCounter<srcPipe, dstPipe>::GetNextId() : EventID;
+#endif
+#endif
+
+    PTO_INTERNAL Derived &InitAddr(uint64_t fftsAddr)
+    {
+        return self().InitAddrImpl(fftsAddr);
+    }
+
+    template <uint8_t CrossCoreId = 0xff>
+    PTO_INTERNAL Derived &Wait()
+    {
+#ifndef __PTO_AUTO__
+        return self().template WaitImpl<CrossCoreId>();
+#else
+        return self();
+#endif
+    }
+
+    template <uint8_t CrossCoreId = 0xff>
+    PTO_INTERNAL Derived &Init()
+    {
+#ifndef __PTO_AUTO__
+        return self().template InitImpl<CrossCoreId>();
+#else
+        return self();
+#endif
+    }
+
+    template <uint8_t CrossCoreId = 0xff>
+    PTO_INTERNAL Derived &Record()
+    {
+        return Init<CrossCoreId>();
+    }
+
+    PTO_INTERNAL EventBase() = default;
+    PTO_INTERNAL EventBase(RecordEvent)
+    {
+        Init();
+    }
+
+    PTO_INTERNAL Derived &operator=(RecordEvent)
+    {
+        return Init();
+    }
+
+private:
+    PTO_INTERNAL Derived &self()
+    {
+        return *static_cast<Derived *>(this);
+    }
+};
+
 } // namespace pto
 #endif

include/pto/common/pto_instr.hpp
@@ -1758,7 +1758,7 @@ PTO_INST RecordEvent TEXP(TileDataDst &dst, TileDataSrc &src, WaitEvents &...eve
 
 template <auto PrecisionType = PowAlgorithm::DEFAULT, typename DstTile, typename BaseTile, typename ExpTile,
           typename TmpTile, typename... WaitEvents>
-PTO_INTERNAL RecordEvent TPOW(DstTile &dst, BaseTile &base, ExpTile &exp, TmpTile &tmp, WaitEvents &...events)
+PTO_INST RecordEvent TPOW(DstTile &dst, BaseTile &base, ExpTile &exp, TmpTile &tmp, WaitEvents &...events)
 {
     TSYNC(events...);
     MAP_INSTR_IMPL_T(TPOW, PTO_TEMPLATE_ARGS(PrecisionType), dst, base, exp, tmp);
@@ -1767,8 +1767,8 @@ PTO_INTERNAL RecordEvent TPOW(DstTile &dst, BaseTile &base, ExpTile &exp, TmpTil
 
 template <auto PrecisionType = PowAlgorithm::DEFAULT, typename DstTile, typename BaseTile, typename TmpTile,
           typename... WaitEvents>
-PTO_INTERNAL RecordEvent TPOWS(DstTile &dst, BaseTile &base, typename DstTile::DType exp, TmpTile &tmp,
-                               WaitEvents &...events)
+PTO_INST RecordEvent TPOWS(DstTile &dst, BaseTile &base, typename DstTile::DType exp, TmpTile &tmp,
+                           WaitEvents &...events)
 {
     TSYNC(events...);
     MAP_INSTR_IMPL_T(TPOWS, PTO_TEMPLATE_ARGS(PrecisionType), dst, base, exp, tmp);
@@ -2214,6 +2214,7 @@ PTO_INST RecordEvent TFMOD(TileDataDst &dst, TileDataSrc0 &src0, TileDataSrc1 &s
     return {};
 }
 
+#ifndef PTO_COMM_NOT_SUPPORTED
 template <typename Pipe, typename TileProd, TileSplitAxis Split, std::enable_if_t<is_tile_data_v<TileProd>, int> = 0,
           typename... WaitEvents>
 PTO_INST RecordEvent TPUSH(Pipe &pipe, TileProd &tile, WaitEvents &...events)
@@ -2341,6 +2342,8 @@ PTO_INST RecordEvent TFREE(Pipe &pipe, WaitEvents &...events)
     TFREE_IMPL<Pipe>(pipe);
     return {};
 }
+#endif
+
 #if defined(PTO_NPU_ARCH_A5) || defined(PTO_NPU_ARCH_KIRIN9030) || defined(__CPU_SIM)
 template <HistByte byte, typename TileDataDst, typename TileDataSrc, typename TileDataIdx, typename... WaitEvents>
 PTO_INST RecordEvent THISTOGRAM(TileDataDst &dst, TileDataSrc &src, TileDataIdx &idx, WaitEvents &...events)

commit ab06a212fb84646a95ead69a68b2004471dd068f

fix(a2a3): correct TCOLEXPAND pipe from PIPE_V to PIPE_MTE1
TCOLEXPAND (plain broadcast) uses pto_copy_ubuf_to_ubuf (copy engine /
PIPE_MTE1) in its a2a3 implementation, not a pure vector-unit op.
A pipe_barrier(PIPE_V) after TCOLEXPAND does NOT wait for the copy-engine
broadcast to complete, so a subsequent vector read can see stale data.

This is a regression from commit 021789c0 ("refactor: extract Event CRTP
base class and replace opPipeList with OpPipeEntry template"), which
tagged all expand-family ops as PIPE_V. Before that refactoring,
TCOLEXPAND was not in the old opPipeList and defaulted to PIPE_ALL.

All other TCOLEXPAND* / TROWEXPAND* variants use vector instructions
(vadd, vdiv, vmul, vector_dup, vbrcb, etc.) and remain correctly on
PIPE_V.

Co-authored-by: Vladimir Loncar <vloncar@users.noreply.github.com>

include/pto/common/event.hpp
@@ -265,7 +265,7 @@ PTO_DEFINE_OP_PIPE(Op::TLOG, PIPE_V);
 PTO_DEFINE_OP_PIPE(Op::TRECIP, PIPE_V);
 PTO_DEFINE_OP_PIPE(Op::TCMP, PIPE_V);
 PTO_DEFINE_OP_PIPE(Op::TSCATTER, PIPE_V);
-PTO_DEFINE_OP_PIPE(Op::TCOLEXPAND, PIPE_V);
+PTO_DEFINE_OP_PIPE(Op::TCOLEXPAND, PIPE_MTE1);
 PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDDIV, PIPE_V);
 PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDMUL, PIPE_V);
 PTO_DEFINE_OP_PIPE(Op::TCOLEXPANDADD, PIPE_V);

tests/npu/a2a3/src/st/testcase/CMakeLists.txt
@@ -189,6 +189,7 @@ tstore_mat2gm
 tcolscatter
 tscatter
 tcolexpand
+tcolexpand_pipe_bug_mfe
 ttri
 tnot
 trelu

tests/npu/a2a3/src/st/testcase/tcolexpand_pipe_bug_mfe/CMakeLists.txt
@@ -1,0 +1,11 @@
+# --------------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# --------------------------------------------------------------------------------
+
+pto_vec_st(tcolexpand_pipe_bug_mfe)

tests/npu/a2a3/src/st/testcase/tcolexpand_pipe_bug_mfe/gen_data.py
@@ -1,0 +1,61 @@
+#!/usr/bin/python3
+# coding=utf-8
+# --------------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+# CANN Open Software License Agreement Version 2.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+# --------------------------------------------------------------------------------
+
+import os
+import numpy as np
+
+np.random.seed(23)
+
+
+def gen_golden_data(case_name, param):
+    data_type = param.data_type
+    C = param.C
+    K = param.K
+    validCol = param.validCol
+    validRow = param.validRow
+
+    input_arr = np.random.rand(1, K) * 10
+    input_arr = input_arr.astype(data_type)
+
+    golden = np.zeros((C, K), dtype=data_type)
+
+    input_arr.tofile('input.bin')
+    golden.tofile('golden.bin')
+
+
+class Params:
+    def __init__(self, data_type, C, K, validRow, validCol):
+        self.data_type = data_type
+        self.C = C
+        self.K = K
+        self.validRow = validRow
+        self.validCol = validCol
+
+
+if __name__ == "__main__":
+    case_name_list = [
+        "TcolexpandPipeBugTest.case1",
+    ]
+
+    case_params_list = [
+        Params(np.float32, 32, 32, 32, 32),
+    ]
+
+    for i, case_name in enumerate(case_name_list):
+        if not os.path.exists(case_name):
+            os.makedirs(case_name)
+        original_dir = os.getcwd()
+        os.chdir(case_name)
+
+        gen_golden_data(case_name, case_params_list[i])
+
+        os.chdir(original_dir)

tests/npu/a2a3/src/st/testcase/tcolexpand_pipe_bug_mfe/main.cpp
@@ -1,0 +1,91 @@
+/**
+Copyright (c) 2025 Huawei Technologies Co., Ltd.
+This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+CANN Open Software License Agreement Version 2.0 (the "License").
+Please refer to the License for details. You may not use this file except in compliance with the License.
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+See LICENSE in the root of the software repository for the full text of the License.
+*/
+
+#include "test_common.h"
+#include <gtest/gtest.h>
+#include <acl/acl.h>
+
+using namespace std;
+using namespace PtoTestCommon;
+
+template <typename T, int C, int K, int srcValidCol, int dstValidRow, int dstValidCol>
+void launchTcolexpandPipeBug(T* out, T* src, void* stream);
+
+class TcolexpandPipeBugTest : public testing::Test {
+public:
+protected:
+    void SetUp() override {}
+
+    void TearDown() override {}
+};
+
+std::string GetGoldenDir()
+{
+    const testing::TestInfo* testInfo = testing::UnitTest::GetInstance()->current_test_info();
+    const std::string caseName = testInfo->name();
+    std::string suiteName = testInfo->test_suite_name();
+    std::string fullPath = "../" + suiteName + "." + caseName;
+    return fullPath;
+}
+
+template <typename T, int C, int K, int srcValidCol, int dstValidRow, int dstValidCol>
+bool TestFramework()
+{
+    size_t dstByteSize = C * K * sizeof(T);
+    size_t srcByteSize = 1 * K * sizeof(T);
+
+    aclInit(nullptr);
+    aclrtSetDevice(0);
+
+    aclrtStream stream;
+    aclrtCreateStream(&stream);
+
+    T *dstHost, *srcHost;
+    T *dstDevice, *srcDevice;
+
+    aclrtMallocHost((void**)(&dstHost), dstByteSize);
+    aclrtMallocHost((void**)(&srcHost), srcByteSize);
+
+    aclrtMalloc((void**)&dstDevice, dstByteSize, ACL_MEM_MALLOC_HUGE_FIRST);
+    aclrtMalloc((void**)&srcDevice, srcByteSize, ACL_MEM_MALLOC_HUGE_FIRST);
+
+    ReadFile(GetGoldenDir() + "/input.bin", srcByteSize, srcHost, srcByteSize);
+
+    aclrtMemcpy(srcDevice, srcByteSize, srcHost, srcByteSize, ACL_MEMCPY_HOST_TO_DEVICE);
+    launchTcolexpandPipeBug<T, C, K, srcValidCol, dstValidRow, dstValidCol>(dstDevice, srcDevice, stream);
+
+    aclrtSynchronizeStream(stream);
+    aclrtMemcpy(dstHost, dstByteSize, dstDevice, dstByteSize, ACL_MEMCPY_DEVICE_TO_HOST);
+
+    WriteFile(GetGoldenDir() + "/output.bin", dstHost, dstByteSize);
+
+    aclrtFree(dstDevice);
+    aclrtFree(srcDevice);
+
+    aclrtFreeHost(dstHost);
+    aclrtFreeHost(srcHost);
+
+    aclrtDestroyStream(stream);
+    aclrtResetDevice(0);
+    aclFinalize();
+
+    std::vector<T> golden(dstByteSize);
+    std::vector<T> devFinal(dstByteSize);
+    ReadFile(GetGoldenDir() + "/golden.bin", dstByteSize, golden.data(), dstByteSize);
+    ReadFile(GetGoldenDir() + "/output.bin", dstByteSize, devFinal.data(), dstByteSize);
+
+    return ResultCmp(golden, devFinal, 0.001f);
+}
+
+TEST_F(TcolexpandPipeBugTest, case1)
+{
+    bool ret = TestFramework<float, 32, 32, 32, 32, 32>();
+    EXPECT_TRUE(ret);
+}

tests/npu/a2a3/src/st/testcase/tcolexpand_pipe_bug_mfe/tcolexpand_pipe_bug_mfe_kernel.cpp
@@ -1,0 +1,73 @@
+/**
+Copyright (c) 2025 Huawei Technologies Co., Ltd.
+This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+CANN Open Software License Agreement Version 2.0 (the "License").
+Please refer to the License for details. You may not use this file except in compliance with the License.
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+See LICENSE in the root of the software repository for the full text of the License.
+*/
+
+#include <pto/pto-inst.hpp>
+#include <pto/common/constants.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace pto;
+
+template <typename T, int C, int K, int srcValidCol, int dstValidRow, int dstValidCol>
+__global__ AICORE void runTcolexpandPipeBug(__gm__ T __out__* out, __gm__ T __in__* src)
+{
+    using DynShape = Shape<1, 1, 1, -1, -1>;
+    using DynStride = Stride<1, 1, -1, -1, 1>;
+    using GlobalData = GlobalTensor<T, DynShape, DynStride>;
+
+    GlobalData srcGlobal(src, DynShape(1, srcValidCol), DynStride(1, K));
+    GlobalData dstGlobal(out, DynShape(dstValidRow, dstValidCol), DynStride(C, K));
+
+    using SrcTile = Tile<TileType::Vec, T, 1, K, BLayout::RowMajor, -1, -1>;
+    SrcTile srcTile(1, srcValidCol);
+
+    using DstTile = Tile<TileType::Vec, T, C, K, BLayout::RowMajor, -1, -1>;
+    DstTile dstTile(dstValidRow, dstValidCol);
+
+    using SubTile = Tile<TileType::Vec, T, C, K, BLayout::RowMajor, -1, -1>;
+    SubTile subTile(dstValidRow, dstValidCol);
+
+    TASSIGN(srcTile, 0x0);
+    TASSIGN(dstTile, 1 * K * sizeof(T));
+    TASSIGN(subTile, (1 + C) * K * sizeof(T));
+
+    TLOAD(srcTile, srcGlobal);
+
+#ifndef __PTO_AUTO__
+    set_flag(PIPE_MTE2, PIPE_V, EVENT_ID0);
+    wait_flag(PIPE_MTE2, PIPE_V, EVENT_ID0);
+#endif
+
+    TCOLEXPAND(dstTile, srcTile);
+
+    pipe_barrier(PIPE_V);
+
+    TSUB(subTile, dstTile, srcTile);
+
+#ifndef __PTO_AUTO__
+    set_flag(PIPE_V, PIPE_MTE3, EVENT_ID0);
+    wait_flag(PIPE_V, PIPE_MTE3, EVENT_ID0);
+#endif
+
+    TSTORE(dstGlobal, subTile);
+    out = dstGlobal.data();
+}
+
+template <typename T, int C, int K, int srcValidCol, int dstValidRow, int dstValidCol>
+void launchTcolexpandPipeBug(T* out, T* src, void* stream)
+{
+    cout << "launchTcolexpandPipeBug start!" << endl;
+
+    runTcolexpandPipeBug<T, C, K, srcValidCol, dstValidRow, dstValidCol><<<1, nullptr, stream>>>(out, src);
+
+    cout << "launchTcolexpandPipeBug end!" << endl;
+}
+
+template void launchTcolexpandPipeBug<float, 32, 32, 32, 32, 32>(float*, float*, void*);